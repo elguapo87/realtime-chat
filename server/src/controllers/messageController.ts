32d31b6b4f816e2077bc5e2c7dd72619576b695c
elguapo87 <pglogovac@gmail.com>
2025-07-06 19:33:06 +0200

    Created getUsersForSidebar & getAllMessagesForSelectedUser API routes

--- server/src/controllers/messageController.ts
+++ server/src/controllers/messageController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { UserDocument } from "../models/userModel";
+import userModel, { UserDocument } from "../models/userModel";
 import cloudinary from "../lib/cloudinary";
 import messageModel from "../models/messageModel";
 
@@ -41,11 +41,60 @@ export const sendMessage = async (req: AuthenticatedRequest, res: Response) => {
 export const markMessageAsSeen = async (req: AuthenticatedRequest, res: Response) => {
     try {
         const { id } = req.params;
-        
+
         await messageModel.findByIdAndUpdate(id, { seen: true });
 
         res.json({ success: true });
-        
+
+    } catch (error) {
+        const errMessage = error instanceof Error ? error.message : "An unknown error occurred";
+        res.json({ success: false, message: errMessage });
+    }
+};
+
+
+export const getUsersForSidebar = async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const userId = req.user?._id;
+        const filteredUsers = await userModel.find({ _id: { $ne: userId } }).select("-password");
+
+        // Count number of messages not seen
+        const unseenMessages: Record<string, number> = {};
+        const promises = filteredUsers.map(async (user) => {
+            const messages = await messageModel.find({ senderId: user._id, receiverId: userId, seen: false });
+            if (messages.length > 0) {
+                unseenMessages[user._id] = messages.length;
+            }
+        });
+
+        await Promise.all(promises);
+
+        res.json({ success: true, users: filteredUsers, unseenMessages });
+
+    } catch (error) {
+        const errMessage = error instanceof Error ? error.message : "An unknown error occurred";
+        res.json({ success: false, message: errMessage });
+    }
+};
+
+export const getAllMessagesForSelectedUser = async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const { id: selectedUserId } = req.params;
+        const myId = req.user?._id;
+
+        const messages = await messageModel.find({
+            $or: [
+                { senderId: myId, receiverId: selectedUserId },
+                { senderId: selectedUserId, receiverId: myId }
+            ]
+        });
+
+        await messageModel.updateMany({ senderId: selectedUserId, receiverId: myId }, {
+            seen: true
+        });
+
+        res.json({ success: true, messages });
+
     } catch (error) {
         const errMessage = error instanceof Error ? error.message : "An unknown error occurred";
         res.json({ success: false, message: errMessage });
--- server/src/routes/messageRoutes.ts
+++ server/src/routes/messageRoutes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/authUser";
-import { markMessageAsSeen, sendMessage } from "../controllers/messageController";
+import { getAllMessagesForSelectedUser, getUsersForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController";
 
 const router = express.Router();
 
 router.post("/send/:id", protectRoute, sendMessage);
 router.post("/mark/:id", protectRoute, markMessageAsSeen);
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/messages/:id", protectRoute, getAllMessagesForSelectedUser);
 
 export default router;
